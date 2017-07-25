70c9a5181e9c5c898757f40f5c140a5bc381398c
Higor César Ramos <higor.crr@gmail.com>
2017-07-25 21:33:23 +0200

    Lonely integer(no bitwise operation) (#3)

---
+++ ctci-lonely-integer/CSharp/LonelyInteger/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LonelyInteger
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            string[] a_temp = Console.ReadLine().Split(' ');
+            var uniqueNumbers = new List<int>();
+            var lonelyInteger = 0;
+            foreach (var number in a_temp.Select(x => Convert.ToInt32(x)))
+            {
+                if (uniqueNumbers.Contains(number))
+                    lonelyInteger -= number;
+                else
+                {
+                    lonelyInteger += number;
+                    uniqueNumbers.Add(number);
+                }
+            }
+            Console.WriteLine(lonelyInteger);
+        }
+    }
+}
