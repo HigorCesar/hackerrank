60d982b93131919eeb6944cb1d455230f377092b
Higor César Ramos <higor.crr@gmail.com>
2017-07-29 22:38:12 +0200

    ctci-staircase (#4)

---
+++ ctci-recursive-staircase/CSharp/Ctci.RecursiveStaircase/Tests..cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace Ctci.RecursiveStaircase
+{
+    public class Tests
+    {
+        [Fact]
+        public void CountWays_1_equals_1()
+        {
+            Assert.Equal(1, RecursiveStaircase.Fib(1));
+        }
+        [Fact]
+        public void CountWays_3_equals_4()
+        {
+            Assert.Equal(4, RecursiveStaircase.Fib(3));
+        }
+        [Fact]
+        public void CountWays_7_equals_44()
+        {
+            Assert.Equal(44, RecursiveStaircase.Fib(7));
+        }
+    }
+}
