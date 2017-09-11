60026715a7bb2fdeeb2814a55bf99ec7ca57d623
Higor Ramos <higor.crr@gmail.com>
2017-09-11 20:30:30 +0200

    wip

--- min-max-sum/Csharp/MinMaxSolverTests.cs
+++ min-max-sum/Csharp/MinMaxSolverTests.cs
@@ -6,23 +6,29 @@ namespace mini_max_sum
 {
     class MinMaxSolver
     {
-        public static Int64 MinSum(int[] values){
+        public static void Solve(decimal[] values)
+        {
+            System.Console.WriteLine($"{MaxSum(values)} {MinSum(values)}");
+        }
+        public static decimal MinSum(decimal[] values)
+        {
             Sort(values);
             return values.Skip(1).Sum();
         }
-         public static Int64 MaxSum(int[] values){
+        public static decimal MaxSum(decimal[] values)
+        {
             Sort(values);
             return values.Take(4).Sum();
         }
-        public static void Sort(int[] values)
+        public static void Sort(decimal[] values)
         {
             Sort(values, values.Length);
         }
-        public static void Sort(int[] values, int length)
+        public static void Sort(decimal[] values, int length)
         {
             var pivot = values[length - 1];
-            var lowerThanPivot = new int[length];
-            var biggerThanPivot = new int[length];
+            var lowerThanPivot = new decimal[length];
+            var biggerThanPivot = new decimal[length];
             int lowerThanPivotCount = 0;
             int biggerThanPivotCount = 0;
             for (int i = 0; i < length - 1; i++)
@@ -48,23 +54,30 @@ namespace mini_max_sum
 
         }
     }
-    public class UnitTest1
+    public class MinMaxSolverTests
     {
         [Fact]
         public void Sort3Items()
         {
-            var actual = new[] { 3, 1, 2 };
+            var actual = new decimal[] { 3, 1, 2 };
             MinMaxSolver.Sort(actual);
-            Assert.Equal(new[] { 1, 2, 3 }, actual);
+            Assert.Equal(new decimal[] { 1, 2, 3 }, actual);
 
         }
         [Fact]
         public void Sort10Items()
         {
-            var actual = new[] { 3, 1, 2, 0, 99, 2, 54, 67, 888, 4 };
+            var actual = new decimal[] { 3, 1, 2, 0, 99, 2, 54, 67, 888, 4 };
             MinMaxSolver.Sort(actual);
-            Assert.Equal(new[] { 0,1, 2, 2,3,4,54,67,99,888 }, actual);
+            Assert.Equal(new decimal[] { 0, 1, 2, 2, 3, 4, 54, 67, 99, 888 }, actual);
+        }
 
+        [Fact]
+        public void MaxSum()
+        {
+            var actual = new decimal[] { 140638725, 436257910, 953274816, 734065819, 362748590 };
+            Assert.Equal((decimal)1673711044, MinMaxSolver.MaxSum(actual));
+            Assert.Equal((decimal)2486347135, MinMaxSolver.MinSum(actual));
         }
     }
 }
