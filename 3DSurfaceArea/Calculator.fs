e65e405612a55356843818c32ab25086f34032fd
Higor César Ramos <higor.crr@gmail.com>
2018-05-09 15:38:10 +0200

    mutation free

--- 3DSurfaceArea/Calculator.fs
+++ 3DSurfaceArea/Calculator.fs
@@ -1,12 +1,10 @@
 module Calculator
 open System
 let private cubeArea = 6
-let CalculateByCube (surfaceArea: int[,]) pos =
-    let line,col = pos
-    surfaceArea.[line,col] * cubeArea - 2*(surfaceArea.[line,col]-1)
+let CalculateByCube (surfaceArea: int[,]) row column =
+    surfaceArea.[row,column] * cubeArea - 2*(surfaceArea.[row,column]-1)
 
-let neighbors  (surfaceArea: int[,]) (pos :int*int) =
-    let row,column = pos
+let neighbors  (surfaceArea: int[,]) row column =
     let neighbor nRow nColumn =
         if nRow < 0 || nRow >= Array2D.length1 surfaceArea ||
            nColumn < 0 ||  nColumn >= Array2D.length2 surfaceArea then
@@ -22,14 +20,13 @@ let neighbors  (surfaceArea: int[,]) (pos :int*int) =
     neighbor row (column - 1) +
     neighbor row (column + 1)
 
-let Calculate (surfaceArea: int[,]) pos =
-    CalculateByCube surfaceArea pos - neighbors surfaceArea pos
+let Calculate (surfaceArea: int[,]) row column =
+    CalculateByCube surfaceArea row column - neighbors surfaceArea row column
 
 let CalculateWholeCube (surfaceArea: int[,]) =
-    let mutable wholeValue = 0
-    let calc r c v =
-        wholeValue <- wholeValue + Calculate surfaceArea (r,c)
-    surfaceArea|> Array2D.iteri calc
-    wholeValue
+    [ for y in 0..Array2D.length1 surfaceArea-1 do
+        for x in 0..Array2D.length2 surfaceArea-1 do
+            yield Calculate surfaceArea y x ]
+    |> List.sum
 
 
--- 3DSurfaceArea/Tests.fs
+++ 3DSurfaceArea/Tests.fs
@@ -1,6 +1,4 @@
 module Tests
-open Calculator
-open System
 open Xunit
 
 [<Theory>]
@@ -9,50 +7,50 @@ open Xunit
 [<InlineData(3,14)>]
 let ``Calculate when size equals x`` size expectedArea =
     let cube = Array2D.create 1 1 size
-    let actual = Calculator.Calculate cube (0,0)
+    let actual = Calculator.Calculate cube 0 0
     Assert.Equal(expectedArea,actual)
 
 [<Fact>]
 let ``Calculate when size equals one but there is one neighbor on the left``() = 
      let cube = Array2D.create 1 2 1
-     let actual = Calculator.Calculate cube (0,1)
+     let actual = Calculator.Calculate cube 0 1
      Assert.Equal(5,actual)
 
 [<Fact>]
 let ``Calculate when size equals one and there is one neighbor on the right``() = 
      let cube = Array2D.create 1 2 1
-     let actual = Calculator.Calculate cube (0,0)
+     let actual = Calculator.Calculate cube 0 0
      Assert.Equal(5,actual)
 
 [<Fact>]
 let ``Calculate when size equals one and there is one neighbor on the left``() = 
      let cube = Array2D.create 1 2 1
-     let actual = Calculator.Calculate cube (0,1)
+     let actual = Calculator.Calculate cube 0 1
      Assert.Equal(5,actual)
 
 [<Fact>]
 let ``Calculate when size equals one and there is one neighbor one line before``() = 
      let cube = Array2D.create 2 1 1
-     let actual = Calculator.Calculate cube (1,0)
+     let actual = Calculator.Calculate cube 1 0
      Assert.Equal(5,actual)
     
 [<Fact>]
 let ``Calculate when size equals one and there is one neighbor one line after``() = 
      let cube = Array2D.create 2 1 1
-     let actual = Calculator.Calculate cube (0,0)
+     let actual = Calculator.Calculate cube 0 0
      Assert.Equal(5,actual)
 
 [<Fact>]
 let ``Calculate when size equals two and neighbor is equals two``() = 
      let cube = Array2D.create 1 2 2
-     let actual = Calculator.Calculate cube (0,0)
+     let actual = Calculator.Calculate cube 0 0
      Assert.Equal(8,actual)
 
 [<Fact>]
 let ``Calculate ignore neighbor size when it is bigger than current cube``() = 
      let cube = Array2D.create 1 2 1
      cube.[0,0] <- 2
-     let actual = Calculator.Calculate cube (0,1)
+     let actual = Calculator.Calculate cube 0 1
      Assert.Equal(5,actual)
 
 [<Fact>]
