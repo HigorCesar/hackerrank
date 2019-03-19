cae82547ed53ba6a8d56328d7880dbfefbf12c42
Higor César Ramos <higor.crr@gmail.com>
2019-03-19 17:20:49 +0100

    Divisible sum pairs (#24)
    * DivisibleSumPair C#
    
    * Fsharp

---
+++ DivisibleSumPairs/Fsharp/DivisibleSumPairs/DivisibleSumPairs/Tests.fs
@@ -0,0 +1,14 @@
+﻿module DivisibleSumPairsTests
+open Xunit
+
+let divisibleSumPairs (n : int) (k : int) (ar : int array) =
+    ar
+    |> Array.mapi (fun index x -> ar |> Array.skip (index + 1) |> Array.map (fun e -> x, e))
+    |> Array.concat
+    |> Array.filter (fun (a, b) -> (a + b) % k = 0)
+    |> Array.length
+
+[<Fact>]
+let ``HackerRank example``() =
+    let values = [| 1; 3; 2; 6; 1; 2 |]
+    Assert.Equal(5, (divisibleSumPairs 6 3 values))
